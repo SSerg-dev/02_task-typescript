b3e02b4482dca79b3131ef02661b0990126c4f89
SSerg-dev <srmail@mail.ru>
2023-10-09 11:35:33 +0300

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { type } from "os";
 import { totalPrice } from "./02_task.js";
 
 const discountPrice = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+
 console.log("🚀 ~ file: index.ts:5 ~ discountPrice:", discountPrice)
 // 6250
+
 
